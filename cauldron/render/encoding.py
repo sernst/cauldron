27226f28bb375f4b8c38da1ec99a354430195277
sernst <swernst@gmail.com>
2016-09-21 21:40:28 +0200

    Improved ComplexEncoder
    Add additional datetime objects to ComplexEncoder for better handling of DataFrame table display.

--- cauldron/render/encoding.py
+++ cauldron/render/encoding.py
@@ -13,8 +13,11 @@ class ComplexJsonEncoder(json.JSONEncoder):
             return value.isoformat()
         elif isinstance(value, datetime.datetime):
             return value.isoformat()
+        elif isinstance(value, datetime.time):
+            return value.isoformat()
+        elif isinstance(value, datetime.timedelta):
+            return value.total_seconds()
         elif isinstance(value, (np.int32, np.int64)):
             return int(value)
-
 
         return json.JSONEncoder.default(self, value)
