9e27df1a0d97591d6c3d120a4b1b431653b31ac7
Scott Ernst <swernst@gmail.com>
2017-02-09 15:15:23 -0600

    Unit Testing
    Add additional unit testing for kernel server status routes. Add Python 3.6 to CI testing.

---
+++ cauldron/test/test_syntax_highlighting.py
@@ -0,0 +1,52 @@
+from unittest import TestCase
+from pygments.lexers.python import Python3Lexer
+from pygments.lexers.python import PythonLexer
+from cauldron.render import syntax_highlighting
+
+
+class TestSyntaxHighlighting(TestCase):
+
+    def test_source(self):
+        """ should retrieve python lexer by source """
+
+        with open(__file__, 'r') as f:
+            contents = f.read()
+
+        lexer = syntax_highlighting.fetch_lexer(contents)
+        self.assertIsInstance(lexer, PythonLexer)
+
+    def test_language_python3(self):
+        """ should retrieve python 3 lexer by language """
+
+        lexer = syntax_highlighting.fetch_lexer('', 'python3')
+        self.assertIsInstance(lexer, Python3Lexer)
+
+    def test_filename_python(self):
+        """ should retrieve python lexer by filename """
+
+        lexer = syntax_highlighting.fetch_lexer('', 'fake', 'test.py')
+        self.assertIsInstance(lexer, PythonLexer)
+
+    def test_mime_type_python(self):
+        """ should retrieve python lexer by filename """
+
+        lexer = syntax_highlighting.fetch_lexer(
+            '',
+            mime_type='application/x-python'
+        )
+        self.assertIsInstance(lexer, PythonLexer)
+
+    def test_unknown_language(self):
+        """ should not retrieve a lexer for an unknown language """
+        lexer = syntax_highlighting.fetch_lexer('', 'lkjasdlkjsad')
+        self.assertIsNone(lexer)
+
+    def test_unknown_everything(self):
+        """ should not retrieve a lexer for an unknown language """
+        lexer = syntax_highlighting.fetch_lexer(
+            source='asdlkasdj',
+            language='lkjasdlkjsad',
+            filename='test.qweoihwq',
+            mime_type='fictional/lasdlkjad'
+        )
+        self.assertIsNone(lexer)
--- cauldron/test/test_version.py
+++ cauldron/test/test_version.py
@@ -1,15 +1,35 @@
+from collections import OrderedDict
 from cauldron.test import support
 from cauldron.test.support import scaffolds
+from cauldron.cli.commands import version
 
 
 class TestVersion(scaffolds.ResultsTest):
-    """
-
-    """
+    """ """
 
     def test_version(self):
-        """
-        """
+        """ should retrieve version info """
 
         r = support.run_command('version')
         self.assertFalse(r.failed, 'should not have failed')
+
+    def test_version_args(self):
+        """ should retrieve version info with additional args """
+
+        r = support.run_command('version --verbose --json')
+        self.assertFalse(r.failed, 'should not have failed')
+
+    def test_pretty_print(self):
+        """ should pretty print the source dictionary """
+
+        data = dict(
+            a=[False, 1, 'two', 3.000001, dict(a=1, b='tester')],
+            b=OrderedDict(a=12, b='hello'),
+            c=dict(a=1, b=2, c=3)
+        )
+
+        result = version.pretty_print(data)
+        self.assertTrue(len(result))
+        self.assertGreater(result.find('hello'), 0)
+        self.assertGreater(result.find('two'), 0)
+        self.assertGreater(result.find('tester'), 0)
