e1f144c7bcf5eb8c0007b65fd311b6da178ca045
Scott Ernst <swernst@gmail.com>
2018-07-27 08:02:58 -0500

    Package Configuration
    Tune-up the package configurations including adding a long description.

--- setup.py
+++ setup.py
@@ -24,6 +24,18 @@ with open(os.path.join(MY_DIRECTORY, 'cauldron', 'settings.json'), 'r') as f:
     settings = json.load(f)
 
 
+def readme():
+    """
+    Returns the contents of the package README.rst.
+    """
+    path = os.path.realpath(os.path.join(
+        os.path.dirname(__file__),
+        'README.rst'
+    ))
+    with open(path) as f:
+        return f.read()
+
+
 def populate_extra_files():
     """
     Creates a list of non-python data files to include in package distribution
@@ -42,14 +54,21 @@ def populate_extra_files():
 
     return out
 
+
 setup(
     name='cauldron-notebook',
     version=settings['version'],
     description='The Unnotebook: Scientific Analysis Environment',
+    long_description=readme(),
+    keywords=[
+        'Data', 'Analysis', 'Visualization',
+        'Interactive', 'Interpreter', 'Shell'
+    ],
     url='https://github.com/sernst/cauldron',
     author='Scott Ernst',
     author_email='swernst@gmail.com',
     license='MIT',
+    platforms='Linux, Mac OS X, Windows',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     package_data={'': populate_extra_files()},
     include_package_data=True,
@@ -61,11 +80,18 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
 
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
 
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Scientific/Engineering :: Information Analysis'
     ],
     install_requires=[
         'pandas',
