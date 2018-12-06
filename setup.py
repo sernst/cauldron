4b6a5933a873901383c66d506fe9670adec103b3
Scott Ernst <swernst@gmail.com>
2018-12-05 21:55:49 -0600

    Plotly Resizing
    Modify Plotly fluid resize to support changed dom structure recently introduced.

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
 
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
